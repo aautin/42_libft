e8bc29e0cdac6b485fa7a6c94c2700eb988911b4
ezagof_home <ezagof@gmail.com>
2023-08-20 11:17:36 +0200

    add the struct of linked lists and the prototypes of functions abt used by linked lists

--- libft.h
+++ libft.h
@@ -21,6 +21,25 @@
 #include <stdio.h>			// to remove, just used in the main here...
 #include <string.h>			// can we really use it ? recode the size_t ?
 
+// linked list's structure :
+typedef struct	s_list
+{
+	void			*content;
+	struct s_list	*next;
+}				t_list;
+
+// linked list's functions :
+// t_list			*ft_lstnew(void *content);
+// t_list			*ft_lstlast(t_list *lst);
+// void				ft_lstadd_front(t_list **alst, t_list *new);
+// void				ft_lstdelone(t_list *lst, void (*del)(void *));
+// void				ft_lstadd_back(t_list **alst, t_list *new);
+// void				ft_lstclear(t_list **lst, void (*del)(void *));
+// void				ft_lstiter(t_list *lst, void (*f)(void *));
+// t_list			*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+// int				ft_lstsize(t_list *lst);
+
+
 int				ft_isalnum(int c);
 int				ft_isalpha(int c);
 int				ft_isascii(int c);
