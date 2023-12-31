32a6883485f148277cf6f65154543a5954a79d54
aautin <ezagof@gmail.com>
2023-12-31 13:57:02 +0100

    add a libft + extended .h (which includes the libft plus project_functions) in the includes folder

---
+++ includes/libft_extended.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft_extended.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aautin <aautin@student.42.fr >             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/10 13:26:06 by aautin            #+#    #+#             */
+/*   Updated: 2023/12/31 13:55:05 by aautin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_EXTENDED_H
+# define LIBFT_EXTENDED_H
+
+# include "get_next_line.h"
+# include "libft.h"
+# include "ft_printf.h"
+
+#endif
