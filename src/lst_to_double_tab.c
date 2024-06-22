443f30085f6c64238f7453a9fa80f670f60fe27c
aautin <ezagof@gmail.com>
2024-06-22 18:00:06 +0200

    fix lst_to_double_tab
    it was copying the lst into a dbl array but in reserve order

--- src/lst_to_double_tab.c
+++ src/lst_to_double_tab.c
@@ -6,7 +6,7 @@
 /*   By: aautin <aautin@student.42.fr >             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/01 18:28:48 by aautin            #+#    #+#             */
-/*   Updated: 2024/06/22 16:30:39 by aautin           ###   ########.fr       */
+/*   Updated: 2024/06/22 17:59:51 by aautin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,9 +34,10 @@ void	**lst_to_double_tab(t_list *lst, int *return_size)
 		tab[tab_size] = NULL;
 	else
 		*return_size = tab_size;
+	tab_size = 0;
 	while (head != NULL)
 	{
-		tab[--tab_size] = head->content;
+		tab[tab_size++] = head->content;
 		head = head->next;
 	}
 	return (tab);
